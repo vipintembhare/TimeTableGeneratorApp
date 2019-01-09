8a68324dbd7b227be68a25d55f19ac3de96fed2f
vipinrtembhare <you@example.com>
2019-01-10 01:15:15 +0530

    Initial commit

---
+++ src/timetablegenerator/TimeTableGenerator.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetablegenerator;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Vipin
+ */
+public class TimeTableGenerator extends JApplet {
+    
+    private static final int JFXPANEL_WIDTH_INT = 600;
+    private static final int JFXPANEL_HEIGHT_INT = 600;
+    private static JFXPanel fxContainer;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+                } catch (Exception e) {
+                }
+                
+                JFrame frame = new LoginFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+//                JApplet applet = new TimeTableGenerator();
+//                applet.init();
+//                
+//                frame.setContentPane(applet.getContentPane());
+                
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                
+               // applet.start();
+            }
+        });
+    }
+    
+    @Override
+    public void init() {
+        fxContainer = new JFXPanel();
+        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
+        add(fxContainer, BorderLayout.CENTER);
+        // create JavaFX scene
+        Platform.runLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                createScene();
+            }
+        });
+    }
+    
+    private void createScene() {
+        
+        Button btn = new Button();
+        btn.setText("Login");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        fxContainer.setScene(new Scene(root));
+    }
+    
+}
